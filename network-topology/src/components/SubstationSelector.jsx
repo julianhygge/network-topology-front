cd1ada0e7a60a29f86a6b3a54a6641bab656f3aa
Siddharth Jaiswal <siddharth@hygge.energy>
2024-06-15 12:50:50 +0530

    changes

--- network-topology/src/components/SubstationSelector.jsx
+++ network-topology/src/components/SubstationSelector.jsx
@@ -1,40 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { getSubstations } from '../services/Substation';
 
 const SubstationSelector = ({ setSelectedSubstation }) => {
     const [substations, setSubstations] = useState([]);
     const [selectedSubstationId, setSelectedSubstationId] = useState('');
 
+
     useEffect(() => {
         const fetchSubstations = async () => {
-            const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTgxMDE2MDgsImp0aSI6ImQwMzQ1OWM0LWJmZDktNDVmZS04MTI5LWY0YjA0NTRjN2JiOSIsImV4cCI6MTczMTA2MTYwOCwidXNlciI6Ijk0NTIyYTBhLWM4ZjEtNDBmOC1hMmU1LTlhZWQyZGMwMDAxMCIsInJvbGUiOlsiQ29uc3VtZXIiXSwicGVybWlzc2lvbnMiOlsicmV0cmlldmUtYmlkcyIsImRlbGV0ZS1iaWRzIiwicmV0cmlldmUtdXNlcnMiLCJyZXRyaWV2ZS10cmFuc2FjdGlvbnMiLCJjcmVhdGUtYmlkcyIsInVwZGF0ZS1iaWRzIiwic2VhcmNoLWJpZHMiXX0.tAMQrhw26ZJ385oeLSoLIpLwr9pheiGSygku-jny1fc"
- 
             try {
-                const response = await axios.get('https://hyggedev.ddns.net:8000/net-topology-api/v1/substations',{
-                    headers: {
-                        'Authorization': `Bearer ${token}`
-                      }
-                });
-                console.log('Substations response:', response.data);
-                if (response.data && Array.isArray(response.data.items)) {
-                    setSubstations(response.data.items);
+                const fetchedSubstations = await getSubstations();
+
+                if (fetchedSubstations && Array.isArray(fetchedSubstations.items)) {
+                    setSubstations(fetchedSubstations.items);
                 } else {
-                    console.error('API response is not an array:', response.data);
+                    console.error('Substations data is not an array:', fetchedSubstations);
                 }
             } catch (error) {
                 console.error('Error fetching substations:', error);
             }
         };
 
-        fetchSubstations();
+        fetchSubstations(); 
     }, []);
 
     const handleSubstationChange = (event) => {
         const substationId = event.target.value;
         setSelectedSubstationId(substationId);
         setSelectedSubstation(substationId);
-    };
-
+    }
     return (
         <div>
             <label htmlFor="substation-select">Select Substation:</label>
