b2e3a1920ec01b711afe3519702ad3dec164b284
ishankashyap <ikashyap@ualberta.ca>
2024-07-16 16:21:26 -0700

    rough design

--- network-topology/src/components/TransformerForm.jsx
+++ network-topology/src/components/TransformerForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { updateTransformerData } from "../services/Tranformer";
 
-const TransformerForm = ({ transformer, onSave }) => {
+const TransformerForm = ({ transformer, onSave, onClose }) => {
   const [formData, setFormData] = useState({
     max_capacity_kw: transformer.max_capacity_kw || "",
     export_efficiency: transformer.export_efficiency || "",
@@ -37,39 +37,142 @@ const TransformerForm = ({ transformer, onSave }) => {
     }
   };
 
+  const handleClose = () => {
+    onClose();
+  }
+
   return (
-    <div>
-      <h2>Transformer Details</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Max Capacity (kW):</label>
-          <input  className="border"
-            type="text"
-            name="max_capacity_kw"
-            value={formData.max_capacity_kw}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Export Efficiency:</label>
-          <input  className="border"
-            type="text"
-            name="export_efficiency"
-            value={formData.export_efficiency}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Allow Export:</label>
-          <input  className="border"
-            type="checkbox"
-            name="allow_export"
-            checked={formData.allow_export}
-            onChange={handleChange}
-          />
-        </div>
-        <button  className='border'type="submit">Save</button>
-      </form>
+    <div className="fixed inset-0 flex items-center justify-center bg-customBackground bg-opacity-55 z-50">
+      <div className="bg-white rounded-2xl p-32 w-full max-w-5xl border border-solid shadow-sm max-md:px-5 mt-20 mb-20">
+      <button className="cursor-pointer float-right" onClick={handleClose}>close</button>
+      <h2 className="text-lg font-semibold text-center">Enter/Edit the Transformer Configurations</h2>
+        <form onSubmit={handleSubmit} className="mt-4">
+          <div className="grid grid-cols-2 gap-4">
+            <div className="flex flex-col mb-4 pr-4 border-r border-customBorderColor border-1">
+              <div className="mb-4 flex flex-col items-center">
+                <div className="w-full flex justify-start ml-1">
+                  <label className="block text-customGrey-700 text-[14px]">Name of Transformer</label>
+                </div>
+                <input
+                  className="border border-black rounded-xl w-[300px] py-2 px-3 mt-1"
+                  type="text"
+                  name="name"
+                  value={formData.name || ""}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="mb-4 flex flex-col items-center">
+                <div className="w-full flex justify-start ml-1">
+                  <label className="block text-customGrey-700 text-[14px]">Max Capacity [kW]</label>
+                </div>
+                <input
+                  className="border border-black rounded-xl w-[300px] py-2 px-3 mt-1"
+                  type="text"
+                  name="max_capacity_kw"
+                  value={formData.max_capacity_kw}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="mb-4 flex flex-col items-center">
+                <div className="w-full flex justify-start ml-1">
+                  <label className="block text-customGrey-700 text-[14px]">Years of Service</label>
+                </div>
+                <input
+                  className="border border-black rounded-xl w-[300px] py-2 px-3 mt-1"
+                  type="text"
+                  name="years_of_service"
+                  value={formData.years_of_service || ""}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="mb-4 flex flex-col items-center">
+                <div className="w-full flex justify-start ml-1">
+                  <label className="block text-customGrey-700 text-[14px]">Forward Efficiency</label>
+                </div>
+                <input
+                  className="border border-black rounded-xl w-[300px] py-2 px-3 mt-1"
+                  type="text"
+                  name="years_of_service"
+                  value={formData.forward_efficiency || ""}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div className="flex flex-col mb-4 pl-4">
+              <div className="grid grid-cols-2 mb-7.625 mt-8">
+                <div className="flex items-center">
+                  <input
+                    className="mr-2"
+                    type="checkbox"
+                    name="allow_export"
+                    checked={formData.allow_backward}
+                    onChange={handleChange}
+                    style={{ width: "16px", height: "16px" }}
+                  />
+                  <label className="text-customGrey-700 text-[14px] whitespace-nowrap">Allow Backward</label>
+                </div>
+
+                <div className="flex items-center">
+                  <input
+                    className="mr-2"
+                    type="checkbox"
+                    name="allow_export"
+                    checked={formData.digital_twin_model}
+                    onChange={handleChange}
+                    style={{ width: "16px", height: "16px" }}
+                  />
+                  <label className="text-customGrey-700 text-[14px]">Digital Twin Model</label>
+                </div>
+              </div>
+              <div className="mb-4 flex flex-col items-center">
+                <div className="w-full flex justify-start ml-1">
+                  <label className="block text-customGrey-700 text-[14px]">Backward Efficiency [%]</label>
+                </div>
+                <input
+                  className="border border-black rounded-xl w-[300px] py-2 px-3 mt-1"
+                  type="text"
+                  name="backward_efficiency"
+                  value={formData.backward_efficiency}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="mb-4 flex flex-col items-center">
+                <div className="w-full flex justify-start ml-1">
+                  <label className="block text-customGrey-700 text-[14px]">Primary Ampacity [Ampere]</label>
+                </div>
+                <input
+                  className="border border-black rounded-xl w-[300px] py-2 px-3 mt-1"
+                  type="text"
+                  name="primary_ampacity"
+                  value={formData.primary_ampacity}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="mb-4 flex flex-col items-center">
+                <div className="w-full flex justify-start ml-1">
+                  <label className="block text-customGrey-700 text-[14px]">Secondary Ampacity [Ampere]</label>
+                </div>
+                <input
+                  className="border border-black rounded-xl w-[300px] py-2 px-3 mt-1"
+                  type="text"
+                  name="secondary_ampacity"
+                  value={formData.secondary_ampacity}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+          </div>
+        </form>
+          <button 
+          type="submit" 
+          className="bg-yellow-500 text-white font-semibold py-2 px-4 rounded w-full hover:bg-yellow-600"
+          >
+            Save
+          </button>
+      </div>
     </div>
   );
 };
--- network-topology/tailwind.config.js
+++ network-topology/tailwind.config.js
@@ -8,11 +8,18 @@ module.exports = {
       colors: {
         customGreen: '#49AC82',
         customYellow: '#FFBE21',
+        customBackground: '#BED2D4',
+        customGrey: '#204A56',
+        customBorderColor: '#C0C0C0',
         navColor:"#204A56",
         backPage:"#F5F5F5",
         sideBar:"#1A5A6C",
         gridColor:"#794C03",
         gridColor1:"#E4A83A",
+      },
+      spacing: {
+        '7.75': '1.9375rem', 
+        '7.625': '1.90625rem'
       },
     },
   },
