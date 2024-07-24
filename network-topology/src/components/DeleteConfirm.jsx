d433a113c66b305da6d84d2126d84776a7dd0696
ishankashyap <ikashyap@ualberta.ca>
2024-07-23 19:27:53 -0700

    delete house modal with breadcrumb

--- network-topology/src/components/DeleteConfirm.jsx
+++ network-topology/src/components/DeleteConfirm.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Breadcrumb from "./Breadcrumb";
+import "./Breadcrumb.css"
 
 const Delete = ({ onConfirm, onClose, entityId, entityName, entityType }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="relative flex flex-col justify-center px-1 text-xl font-medium text-orange-600 w-[700px] max-w-[700px] bg-white rounded-3xl border border-sky-400 border-solid shadow-sm z-10">
-      <div className="absolute inset-0 top-4 -z-10 text-[16px] text-black font-light">
-        <Breadcrumb nodeId={entityId}/>
+      <div className="relative flex flex-col justify-center px-1 text-xl font-medium text-orange-600 w-[800px] max-w-[800px] bg-white rounded-3xl border border-sky-400 border-solid shadow-sm z-10">
+      <div className="absolute inset-0 top-5 -z-10 text-[14px] text-black font-light">
+        <Breadcrumb nodeId={entityId} size='small'/>
       </div>
         <div className="flex flex-col items-center  pb-14 pt-2  max-md:pr-5 max-md:max-w-full">
           <button className="cursor-pointer absolute top-3 right-5 p-2 text-4xl font-thin text-black" onClick={onClose}>X</button>
