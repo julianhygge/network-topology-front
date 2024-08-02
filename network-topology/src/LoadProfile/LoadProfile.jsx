2548bafcdf1972688caaed5a5b36b4fba63b0eb1
Yarik Popov <89220488+Yarik-Popov@users.noreply.github.com>
2024-08-02 11:52:31 -0400

    Extract out load profile logic from UserConfiguration component

---
+++ network-topology/src/LoadProfile/LoadProfile.jsx
@@ -0,0 +1,103 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { fetchLoadProfiles } from "../services/LoadProfile";
+import LoadBuilder from "./LoadBuilder";
+import LoadProfileMenuCustom from "./LoadProfileMenu";
+import LoadProfileFileUpload from "./Page1";
+import LoadProfilesList from "./Page3";
+
+const LoadProfile = ({ setUnsaved, setSelectedButton }) => {
+  const [loadProfiles, setLoadProfiles] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showPageLoad, setShowPageLoad] = useState(false);
+  const [searchParams] = useSearchParams();
+  const houseId = searchParams.get("house_id");
+
+  useEffect(() => {
+    const fetchProfiles = async () => {
+      setIsLoading(true);
+      try {
+        const profiles = await fetchLoadProfiles(houseId);
+        setLoadProfiles(profiles);
+        console.log("Load Profiles userConfig: ", profiles)
+      } catch (error) {
+        console.error("Error fetching load profiles:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchProfiles();
+  }, [houseId])
+
+  const handleUploadSuccess = async () => {
+    setIsLoading(true);
+    try {
+      const profiles = await fetchLoadProfiles(houseId);
+      setLoadProfiles(profiles);
+      setSelectedButton("Load Profile");
+    } catch (error) {
+      console.error("Error fetching load profiles after upload:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleUploadAgain = async (deleteLink) => {
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTgxMDE2MDgsImp0aSI6ImQwMzQ1OWM0LWJmZDktNDVmZS04MTI5LWY0YjA0NTRjN2JiOSIsImV4cCI6MTczMTA2MTYwOCwidXNlciI6Ijk0NTIyYTBhLWM4ZjEtNDBmOC1hMmU1LTlhZWQyZGMwMDAxMCIsInJvbGUiOlsiQ29uc3VtZXIiXSwicGVybWlzc2lvbnMiOlsicmV0cmlldmUtYmlkcyIsImRlbGV0ZS1iaWRzIiwicmV0cmlldmUtdXNlcnMiLCJyZXRyaWV2ZS10cmFuc2FjdGlvbnMiLCJjcmVhdGUtYmlkcyIsInVwZGF0ZS1iaWRzIiwic2VhcmNoLWJpZHMiXX0.tAMQrhw26ZJ385oeLSoLIpLwr9pheiGSygku-jny1fc";
+    console.log(deleteLink)
+    try {
+      const response = await fetch(`https://hygge-test.ddns.net:8080/net-topology-api${deleteLink}`,
+        {
+          method: 'DELETE',
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+
+        });
+      if (!response.ok) {
+        throw new Error('Failed to delete file');
+      }
+      console.log('Deleted file before uploading again');
+      setSelectedButton(null); // Reset selected button to allow user to select an option from the left nav 
+    } catch (error) {
+      console.error("Failed to delete the file:", error);
+    }
+  };
+
+  const handleNoClick = () => {
+    setShowPageLoad(true);
+  };
+
+  const onResetLoadProfile = () => {
+    console.log("Resetting load profile");
+    setShowPageLoad(false);
+  }
+
+  const renderContent = () => {
+    if (showPageLoad) {
+      return <LoadProfileMenuCustom onReset={onResetLoadProfile} setUnsaved={setUnsaved} />;
+    }
+
+    if (isLoading) {
+      return (
+        <div className="flex items-center justify-center h-full text-xl">
+          Loading...
+        </div>
+      );
+    }
+    console.log("LoadProfilesList: ", loadProfiles.items)
+    if (!loadProfiles.items || loadProfiles.items?.length === 0) {
+      return (<LoadProfileFileUpload onUploadSuccess={handleUploadSuccess} onNoClick={handleNoClick} />);
+    }
+
+    return loadProfiles.items[0].source !== "Builder" ? (
+      <LoadProfilesList profiles={loadProfiles} onUploadAgain={handleUploadAgain} />
+    ) : (<LoadBuilder onReset={() => { setLoadProfiles({}); onResetLoadProfile() }} profileId={loadProfiles.items[0].profile_id} setUnsaved={setUnsaved} />)
+  }
+
+  return renderContent();
+}
+
+export default LoadProfile;
