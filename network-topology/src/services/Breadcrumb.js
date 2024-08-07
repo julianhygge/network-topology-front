b8f32f11ce8ce9348ceccef13f71fef4ff2b9277
Yarik Popov <89220488+Yarik-Popov@users.noreply.github.com>
2024-08-07 16:42:02 -0400

    Move api url to config file

--- network-topology/src/services/Breadcrumb.js
+++ network-topology/src/services/Breadcrumb.js
@@ -1,18 +1,17 @@
 import axios from "axios";
-
-const API_URL = "https://hygge-test.ddns.net:8080/net-topology-api/v1";
+import { API_URL } from "services/Config";
 
 export const fetchBreadcrumbNavigationPath = async (node_id) => {
-    const token =
+  const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTgxMDE2MDgsImp0aSI6ImQwMzQ1OWM0LWJmZDktNDVmZS04MTI5LWY0YjA0NTRjN2JiOSIsImV4cCI6MTczMTA2MTYwOCwidXNlciI6Ijk0NTIyYTBhLWM4ZjEtNDBmOC1hMmU1LTlhZWQyZGMwMDAxMCIsInJvbGUiOlsiQ29uc3VtZXIiXSwicGVybWlzc2lvbnMiOlsicmV0cmlldmUtYmlkcyIsImRlbGV0ZS1iaWRzIiwicmV0cmlldmUtdXNlcnMiLCJyZXRyaWV2ZS10cmFuc2FjdGlvbnMiLCJjcmVhdGUtYmlkcyIsInVwZGF0ZS1iaWRzIiwic2VhcmNoLWJpZHMiXX0.tAMQrhw26ZJ385oeLSoLIpLwr9pheiGSygku-jny1fc";
-    try {
+  try {
     const response = await axios.get(`${API_URL}/breadcrumb/nodes/${node_id}/breadcrumb`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
     return response.data;
-    } catch (error) {
+  } catch (error) {
     console.error("Error fetching breadcrumb navigation path details:", error.response);
     throw error;
   }
---
+++ network-topology/src/services/Config.js
@@ -0,0 +1,3 @@
+// const API_URL = "https://hygge-test.ddns.net:8080/net-topology-api/v1";
+const API_URL = "https://localhost:7093/net-topology-api/v1";
+export default API_URL;
