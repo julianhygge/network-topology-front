0befb4d93aeed0afba982bfa50ed2989db4ded26
Yarik Popov <89220488+Yarik-Popov@users.noreply.github.com>
2024-08-07 16:43:51 -0400

    Fix export default in config

--- network-topology/src/services/Config.js
+++ network-topology/src/services/Config.js
@@ -1,3 +1,2 @@
-// const API_URL = "https://hygge-test.ddns.net:8080/net-topology-api/v1";
-const API_URL = "https://localhost:7093/net-topology-api/v1";
-export default API_URL;
+export const API_URL = "https://hygge-test.ddns.net:8080/net-topology-api/v1";
+// export const API_URL = "https://localhost:7093/net-topology-api/v1";
