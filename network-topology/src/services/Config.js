05caf6eca4c600a5a533ef792dd3c0f2054d656f
Siddharth Jaiswal <siddharth@hygge.energy>
2024-08-12 22:40:04 +0530

    otp input

--- network-topology/src/services/Config.js
+++ network-topology/src/services/Config.js
@@ -1,5 +1,5 @@
-// export const API_URL = "https://hygge-test.ddns.net:8080/net-topology-api/v1";
-export const API_URL = "http://localhost:8022/net-topology-api/v1";
+export const API_URL = "https://hygge-test.ddns.net:8080/net-topology-api/v1";
+// export const API_URL = "http://localhost:8022/net-topology-api/v1";
 export const PROJECT_PREFIX = 'simulator';
 export const HTTP_STATUS_CODES = {
     UNAUTHORIZED: 401,
