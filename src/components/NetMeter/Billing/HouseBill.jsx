fe13f73c7fcf5c5c7bc3aa3f123b1d85b53caeda
Julian Arroyave <87373005+julianhygge@users.noreply.github.com>
2025-07-18 01:26:24 +0530

    Update HouseBill.jsx

--- src/components/NetMeter/Billing/HouseBill.jsx
+++ src/components/NetMeter/Billing/HouseBill.jsx
@@ -16,9 +16,9 @@ export default function HouseBill() {
         setIsLoading(true);
         const res = await GetBill(simId);
         if (res && res.length > 0) {
-          setBillData(res[0]); // Displaying the first bill from the simulation result
+          setBillData(res[0]);
         } else {
-          setBillData(null); // No data found
+          setBillData(null);
         }
         setError(null);
       } catch (err) {
@@ -39,7 +39,7 @@ export default function HouseBill() {
     if (typeof num !== "number") {
       return "0.00";
     }
-    return new Intl.NumberFormat("en-IN", {
+    return new Intl.NumberFormat("en-US", {
       minimumFractionDigits: 2,
       maximumFractionDigits: 2,
     }).format(num);
@@ -88,11 +88,10 @@ export default function HouseBill() {
       : "N/A";
 
   return (
-    <div className="bg-[#E7FAFF] min-h-screen">
+    <div className="bg-[#E7FAFF] h-screen">
       <Navbar />
 
-      <div className="bg-white rounded-2xl border shadow-sm p-6 mx-auto mt-10 max-w-6xl">
-        {/* Header */}
+      <div className="bg-white rounded-2xl border shadow-sm h-[80vh] p-6 mt-10 mr-40 ml-40">
         <div className="flex items-center justify-between mb-8 pb-4 border-b border-gray-100">
           <div className="flex items-center space-x-4">
             <div className="w-12 h-12 flex items-center justify-center">
@@ -108,37 +107,34 @@ export default function HouseBill() {
 
           <div className="flex item-center">
             <p className=" text-gray-800 mr-2">Bill ID:</p>
-            <p className=" text-gray-800">{billData.id.slice(0, 13)}</p>
+            <p className=" text-gray-800">INV-2025-00123</p>
           </div>
         </div>
 
-        {/* Two Sidebars Layout */}
-        <div className="flex flex-col md:flex-row gap-6">
-          {/* Sidebar 1 - Account Information & Energy Usage */}
-          <div className="flex-1 bg-gray-50 rounded-xl p-6 border border-gray-200">
-            {/* Account Information Section */}
+        <div className="flex gap-6">
+          <div className="flex-1 bg-gray-100 rounded-xl p-6 border border-gray-100">
             <div className="mb-8">
               <h2 className="text-lg font-semibold text-gray-800 mb-4 pb-2 border-b border-gray-200">
                 Account Information
               </h2>
               <div className="space-y-3">
-                <div className="flex justify-between items-center py-2 border-b border-gray-200">
+                <div className="flex justify-between items-center border-b border-gray-200">
                   <span className="text-sm text-gray-600 ">Customer Name</span>
                   <span className="font-medium text-gray-800">
-                    {bill_details.house_name || "N/A"}
+                    Abhinav Monohar
                   </span>
                 </div>
-                <div className="flex justify-between items-center py-2 border-b border-gray-200">
+                <div className="flex justify-between items-center  border-b border-gray-200">
                   <span className="text-sm text-gray-600">Address</span>
                   <span className="font-medium text-gray-800">
                     Krishna Apartment, Dwaraka, IN 851101
                   </span>
                 </div>
-                <div className="flex justify-between items-center py-2 border-b border-gray-200">
+                <div className="flex justify-between items-center border-b border-gray-200">
                   <span className="text-sm text-gray-600">Customer ID</span>
                   <span className="font-medium text-gray-800">1234567890</span>
                 </div>
-                <div className="flex justify-between items-center py-2 border-b border-gray-200">
+                <div className="flex justify-between items-center border-b border-gray-200">
                   <span className="text-sm text-gray-600">Billing Period</span>
                   <span className="font-medium text-gray-800">
                     {formattedPeriod}
@@ -147,31 +143,36 @@ export default function HouseBill() {
               </div>
             </div>
 
-            {/* Energy Usage Section */}
             <div>
               <h2 className="text-lg font-semibold text-gray-800 mb-4 pb-2 border-b border-gray-200">
                 Energy Usage
               </h2>
               <div className="space-y-3">
-                <div className="flex justify-between items-center py-2 border-b border-gray-200">
+                <div className="flex justify-between items-center border-b border-gray-200">
                   <span className="text-sm text-gray-600">Meter Number</span>
                   <span className="font-medium text-gray-800">MTR-ELE-98762</span>
                 </div>
-                <div className="flex justify-between items-center py-2 border-b border-gray-200">
-                  <span className="text-sm text-gray-600">Total Imported Energy</span>
+                <div className="flex justify-between items-center border-b border-gray-200">
+                  <span className="text-sm text-gray-600">
+                    Total Imported Energy
+                  </span>
                   <span className="font-medium text-gray-800">
                     {formatNumber(total_energy_imported_kwh)} kWh
                   </span>
                 </div>
-                <div className="flex justify-between items-center py-2 border-b border-gray-200">
-                  <span className="text-sm text-gray-600">Total Exported Energy</span>
+                <div className="flex justify-between items-center border-b border-gray-200">
+                  <span className="text-sm text-gray-600">
+                    Total Exported Energy
+                  </span>
                   <span className="font-medium text-gray-800">
-                    {formatNumber(total_energy_exported_kwh)} kWh
+                    {formatNumber(total_energy_exported_kwh)} kWh{" "}
                   </span>
                 </div>
-                <div className="flex justify-between items-center py-2 border-b border-gray-200">
-                  <span className="text-sm text-gray-600">Net Energy Consumed</span>
-                  <span className="font-medium text-gray-800">
+                <div className="flex justify-between items-center border-b border-gray-200">
+                  <span className="text-sm text-gray-600">
+                    Net Energy Consumed
+                  </span>
+                  <span className="font-medium ">
                     {formatNumber(net_energy_balance_kwh)} kWh
                   </span>
                 </div>
@@ -179,66 +180,65 @@ export default function HouseBill() {
             </div>
           </div>
 
-          {/* Sidebar 2 - Bill Summary & Amount Due */}
-          <div className="flex-1 bg-gray-50 rounded-xl p-6 border border-gray-200">
-            {/* Bill Summary Section */}
+          <div className="flex-1 bg-gray-100 rounded-xl p-6 border border-gray-100">
             <div className="mb-8">
               <h2 className="text-lg font-semibold text-gray-800 mb-4 pb-2 border-b border-gray-200">
                 Total Bill Summary
               </h2>
-<div className="space-y-3">
-  {bill_details.policy_type === "SIMPLE_NET" && (
-    <>
-      <div className="flex justify-between items-center py-2 border-b border-gray-200">
-        <span className="text-sm text-gray-600">Energy Charges</span>
-        <span className="font-medium text-gray-800">
-          ₹ {formatNumber(bill_details.energy_charges)}
-        </span>
-      </div>
-    </>
-  )}
+              <div className="space-y-3">
+                {bill_details.policy_type === "SIMPLE_NET" && (
+                  <>
+                    <div className="flex justify-between items-center border-b border-gray-200">
+                      <span className="text-sm text-gray-600">
+                        Retail Charges ({formatNumber(net_energy_balance_kwh)} kWh @ ₹{formatNumber(bill_details.import_retail_price_per_kwh)}/kWh)
+                      </span>
+                      <span className="font-medium text-gray-800">
+                        ₹ {formatNumber(bill_details.energy_charges)}
+                      </span>
+                    </div>
+                  </>
+                )}
 
-  {bill_details.policy_type === "GROSS_METERING" && (
-    <>
-      <div className="flex justify-between items-center py-2 border-b border-gray-200">
-        <span className="text-sm text-gray-600">Retail Charges</span>
-        <span className="font-medium text-gray-800">
-          ₹ {formatNumber(bill_details.imported_energy_charges)}
-        </span>
-      </div>
-      <div className="flex justify-between items-center py-2 border-b border-gray-200">
-        <span className="text-sm text-green-600">Wholesale Credit</span>
-        <span className="font-medium text-green-600">
-          - ₹ {formatNumber(bill_details.exported_energy_credit)}
-        </span>
-      </div>
-    </>
-  )}
-
-  <div className="flex justify-between items-center py-2 border-b border-gray-200">
-    <span className="text-sm text-gray-600">Fixed Charge</span>
-    <span className="font-medium text-gray-800">
-      ₹ {formatNumber(bill_details.fixed_charges)}
-    </span>
-  </div>
-  <div className="flex justify-between items-center py-2 border-b border-gray-200">
-    <span className="text-sm text-gray-600">Tax on Energy</span>
-    <span className="font-medium text-gray-800">
-      ₹ {formatNumber(bill_details.tax_amount_on_energy)}
-    </span>
-  </div>
-</div>
+                {bill_details.policy_type === "GROSS_METERING" && (
+                  <>
+                    <div className="flex justify-between items-center border-b border-gray-200">
+                      <span className="text-sm text-gray-600">
+                        Retail Charges ({formatNumber(total_energy_imported_kwh)} kWh @ ₹{formatNumber(bill_details.import_retail_price_per_kwh)}/kWh)
+                      </span>
+                      <span className="font-medium text-gray-800">
+                        ₹ {formatNumber(bill_details.imported_energy_charges)}
+                      </span>
+                    </div>
+                    <div className="flex justify-between items-center border-b border-gray-200">
+                      <span className="text-sm text-gray-600">
+                        Wholesale Charges ({formatNumber(total_energy_exported_kwh)} kWh @ ₹{formatNumber(bill_details.exp_whole_price_kwh)}/kWh)
+                      </span>
+                      <span className="font-medium text-gray-800">
+                        - ₹ {formatNumber(bill_details.exported_energy_credit)}
+                      </span>
+                    </div>
+                  </>
+                )}
+                <div className="flex justify-between items-center border-b border-gray-200">
+                  <span className="text-sm text-gray-600">Fixed Charge</span>
+                  <span className="font-medium text-gray-800">
+                  ₹ {formatNumber(bill_details.fixed_charges)}
+                  </span>
+                </div>
+                <div className="flex justify-between items-center border-b border-gray-200">
+                  <span className="text-sm text-gray-600">Tax on Energy</span>
+                  <span className="font-medium text-gray-800">₹ {formatNumber(bill_details.tax_amount_on_energy)}</span>
+                </div>
+              </div>
             </div>
 
-            {/* Amount Due Section */}
-            <div className="bg-white rounded-lg p-4 mt-12 flex items-center justify-between border border-gray-200">
+            <div className="rounded-lg p-4 flex items-center justify-between">
               <div>
-                <h2 className="text-lg font-semibold text-gray-900">Total Amount Due</h2>
+                <h2 className="text-lg font-semibold">Total Amount Due</h2>
               </div>
+
               <div>
-                <p className="text-2xl font-bold text-gray-900">
-                  ₹ {formatNumber(calculated_bill_amount)}
-                </p>
+                <p className="text-2xl font-bold">₹ {formatNumber(calculated_bill_amount)}</p>
               </div>
             </div>
           </div>
